93c4fbdd89af7a385e1d05a75d364f912ebea3f2
James Galantino <james_galantino@aol.com>
2021-01-25 23:08:00 -0500

    Added and modified Bootstrap card for current weather div. Pseudocoded in script.js

--- Assets/script.js
+++ Assets/script.js
@@ -13,16 +13,9 @@ var savedCitiesEl = $('#city-list');
 
 // DATA ==========================================
 // CREATE
-// create card (Jumbotron?) for current weather based on city
-// create variable for div element to place current weather info inside
-var currentWeatherInfo = $('<div>');
-// add new current weather info div to current weather info containing div
-currentWeatherEl.append(currentWeatherInfo);
-// add Bootstrap classes to current weather info div
-
-
 // create cards for 5-day weather forecast using jQuery selector
- 
+// add AJAX input to current weather div
+
 // BUILD
 // addClasses (Bootstrap) to forecast cards using jQuery
 // addClasses (Bootstrap) to current weather card/Jumbotron
@@ -33,8 +26,30 @@ currentWeatherEl.append(currentWeatherInfo);
 
 // FUNCTIONS =====================================
 // local storage remembers user's last search
+// AJAX API pull to retrieve info for current weather
+// AJAX API pull to retrieve info for 5-day forecast
 
 // USER INTERACTIONS =============================
 // user types desired city into search
 // user clicks search button and current weather/5-day forecast cards display on right
 // latest search prepended to list below search bar (hold 5-8 at a time..?)
+
+
+
+// // create card (Jumbotron?) for current weather based on city
+// // create variable for div element to place current weather info inside
+// var currentWeatherInfo = $('<div>');
+// // add new current weather info div to current weather info containing div
+// currentWeatherEl.append(currentWeatherInfo);
+// // add Bootstrap classes to current weather info div
+// currentWeatherInfo.addClass('card');
+// // add another div for content within card itself
+// var currentWeatherInfoInner = $('<div>')
+// currentWeatherInfo.append(currentWeatherInfoInner)
+// currentWeatherInfoInner.addClass('card-body');
+
+// // add Bootstrap attributes and values to current weather info div
+// // use object
+// currentWeatherInfo.attr({
+
+// })
